bb6e1b1330427b4c750d477462a8623b170f7995
toqvist <tobias-ahlqvist@outlook.com>
2022-04-29 14:53:35 +0200

    Add to cart button + quantitiy selector implemented

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,14 +1,35 @@
 import React from 'react'
 import Card from '@mui/material/Card'
+import Select from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
+import Button from '@mui/material/Button'
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
-export default function Product({ product }) {
+export default function Product({ product, addToCart }) {
+
     return (
         <div>
             <Card variant="outlined">
-                <img width= "125px" src={product.img}/>
+                <img width="125px" src={product.img} />
                 <h2>{product.name}</h2>
-                <p>{product.price}</p>
-                
+                <p>€{product.price}</p>
+                <Select defaultValue={1}>
+                    <MenuItem value={1}>1</MenuItem>
+                    <MenuItem value={2}>2</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                    <MenuItem value={4}>4</MenuItem>
+                    <MenuItem value={5}>5</MenuItem>
+                    <MenuItem value={6}>6</MenuItem>
+                    <MenuItem value={7}>7</MenuItem>
+                    <MenuItem value={8}>8</MenuItem>
+                    <MenuItem value={9}>9</MenuItem>
+
+                </Select>
+                <Button onClick={() => addToCart()}
+                    variant="contained"
+                    startIcon={<AddShoppingCartIcon />}>
+                    Add to cart</Button>
+
             </Card>
 
         </div>
