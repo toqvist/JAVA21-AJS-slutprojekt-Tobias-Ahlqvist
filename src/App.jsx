29bf1ce3ad552990d965a0fb16775865eac773f1
toqvist <tobias-ahlqvist@outlook.com>
2022-05-05 13:28:15 +0200

    Commented App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,8 @@ import Box from '@mui/material/Box'
 // This project uses Material-UI to style components, MUI components are imported from @mui/material
 // Custom styling for MUI components is done by adding a sx prop to the component
 
+// MUI icons used for icons, these are imported from @mui/icons
+
 // React router is used to route between components,  the routes are defined in the Routes component's children
 // The 'Link' component, in the Nav component is used to link to other components, for the Router
 
@@ -24,6 +26,9 @@ import Box from '@mui/material/Box'
 
 // The products and their information are defined in the ProductsContext
 // These are used by the Shop component to display the products with the Product component
+
+// React spring is installed as a dependency, but is not currently used. 
+// Spring will be used in the future to animate various components.
 
 function App() {
 
